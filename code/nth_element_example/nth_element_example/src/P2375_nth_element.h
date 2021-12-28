7e22cd01ebb14aae31bd608b2743004d89bb49e1
Johan Lundberg <lundberj@gmail.com>
2021-12-28 12:02:22 +0100

    polished reference implementation. First go at require nths type compatibility with data

--- code/nth_element_example/nth_element_example/src/P2375_nth_element.h
+++ code/nth_element_example/nth_element_example/src/P2375_nth_element.h
@@ -5,52 +5,8 @@
 #include<xutility>
 
 namespace P2375_nth_element {
-	namespace R0 {
-
-		/// Implementation of P2375R0, range-of-nths overload for nth_element. Here called 'nth_elements'
-		///	lacks "comp" which is part of the proposal. No range version.
-		template< class RandomAccessIterator, class RandomAccessIterator2 >
-		constexpr void nth_elements(RandomAccessIterator first, RandomAccessIterator2 nth_first, RandomAccessIterator2 nth_last, RandomAccessIterator last)
-		{
-			if (last - first <= 64) {
-				std::sort(first, last);
-				return;
-			}
-			const auto nth_dist = nth_last - nth_first;
-			if (nth_dist == 0 || *nth_first == last) return;
-			const auto nth_mid = nth_first + nth_dist / 2;
-			const auto at_nth_mid = *nth_mid;
-			std::nth_element(first, at_nth_mid, last);
-			nth_elements(first, nth_first, nth_mid, at_nth_mid);
-			if (at_nth_mid != last) {
-				const auto nth_left = std::find_if_not(nth_mid, nth_last,
-					[at_nth_mid](auto v) {return v == at_nth_mid; });
-				nth_elements(at_nth_mid + 1, nth_left, nth_last, last);
-			}
-		}
-	}
-	// 
+	
 	namespace Current{
-
-		template< class RandomAccessIterator, class RandomAccessIterator2 , class Comp>
-		constexpr void nth_elements(RandomAccessIterator first, RandomAccessIterator2 nth_first, RandomAccessIterator2 nth_last, RandomAccessIterator last, Comp comp)
-		{
-			if (last - first <= 64) {
-				std::sort(first, last, comp);
-				return;
-			}
-			const auto nth_dist = nth_last - nth_first;
-			if (nth_dist == 0 || *nth_first == last) return;
-			const auto nth_mid = nth_first + nth_dist / 2;
-			const auto at_nth_mid = *nth_mid;
-			std::nth_element(first, at_nth_mid, last, comp);
-			nth_elements(first, nth_first, nth_mid, at_nth_mid, comp);
-			if (at_nth_mid != last) {
-				const auto nth_left = std::find_if_not(nth_mid, nth_last,
-					[at_nth_mid](auto v) {return v == at_nth_mid; });
-				nth_elements(at_nth_mid + 1, nth_left, nth_last, last, comp);
-			}
-		}
 
 		template< class RandomAccessIterator, class RandomAccessIterator2 >
 		constexpr void nth_elements(RandomAccessIterator first, RandomAccessIterator2 nth_first, RandomAccessIterator2 nth_last, RandomAccessIterator last)
@@ -79,14 +35,17 @@ namespace P2375_nth_element {
 					std::ranges::random_access_range R2,
 					class Comp = std::ranges::less, class Proj = std::identity>
 					requires std::sortable< std::ranges::iterator_t<R>, Comp, Proj>
+					&& std::convertible_to<decltype(*std::declval<R2>().begin()), decltype(std::declval<R>().begin())>
 					constexpr std::ranges::borrowed_iterator_t<R>
 					operator()(R&& r, R2&& nths, Comp comp = {}, Proj proj = {}) const
 				{
+					constexpr auto RevertToSortAt = 64;
+
 					const auto first = std::begin(r);
 					const auto last = std::end(r);
 					const auto nth_first = std::begin(nths);
 					const auto nth_last = std::end(nths);
-					if (last - first <= 64) {
+					if (last - first <= RevertToSortAt) {
 						return std::ranges::sort(r, comp, proj);
 					}
 					const auto nth_dist = nth_last - nth_first;
@@ -96,8 +55,11 @@ namespace P2375_nth_element {
 					(void) std::ranges::nth_element(std::ranges::subrange(first, last), at_nth_mid, comp, proj);
 					(void) Nth_elements_fn::operator()(std::ranges::subrange(first, at_nth_mid), std::ranges::subrange(nth_first, nth_mid), comp, proj);
 					if (at_nth_mid != last) {
-						const auto nth_left = std::find_if_not(nth_mid, nth_last,
-							[at_nth_mid](auto v) {return v == at_nth_mid; });
+						// One could revert linear for: nth_last - nth_mid < some constant to get a
+						// little less overhead, but in practice so much more work is done in the inner calls to nth_element.
+						//   const auto nth_leftA = std::find_if_not(nth_mid, nth_last,
+						// 	 [at_nth_mid](auto v) {return v == at_nth_mid; });
+						const auto nth_left = std::upper_bound(nth_mid, nth_last, at_nth_mid );
 						(void) Nth_elements_fn::operator()(std::ranges::subrange(at_nth_mid + 1, last), std::ranges::subrange(nth_left, nth_last), comp, proj);
 					}
 					return last;
@@ -107,6 +69,7 @@ namespace P2375_nth_element {
 				template<std::random_access_iterator I, std::sentinel_for<I> S,
 					std::ranges::random_access_range R2, class Comp = std::ranges::less, class Proj = std::identity>
 					requires std::sortable<I, Comp, Proj>
+				  && std::convertible_to<decltype(*std::declval<R2>().begin()), I>
 					constexpr I
 					operator()(I first, R2&& nths, S last, Comp comp = {}, Proj proj = {}) const
 				{
@@ -117,6 +80,11 @@ namespace P2375_nth_element {
 
 		}
 
+		template< class RandomAccessIterator, class RandomAccessIterator2, class Comp>
+		constexpr void nth_elements(RandomAccessIterator first, RandomAccessIterator2 nth_first, RandomAccessIterator2 nth_last, RandomAccessIterator last, Comp comp)
+		{
+			return ranges::nth_elements(std::ranges::subrange(first, last), std::ranges::subrange(nth_first, nth_last), comp);
+		}
 
 
   }
